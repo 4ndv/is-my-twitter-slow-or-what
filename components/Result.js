f3139e03859c90061f8b311bc896676e6bd9568c
Andrey Viktorov <andreyviktorov@outlook.com>
2021-03-12 01:17:39 +0700

    Added new logging server with ipv6

--- components/Result.js
+++ components/Result.js
@@ -21,7 +21,7 @@ const Result = ({ test, control, controlTaco }) => {
 
   const logParams = new URLSearchParams({ test: testSpeed.toFixed(2), control: controlSpeed.toFixed(2), controlTaco: controlTacoSpeed.toFixed(2), v: 3 })
 
-  const logUrl = `https://imtsow.lynx.pink/log.png?${logParams.toString()}`
+  const logUrl = `https://imtsow-new.lynx.pink/log.png?${logParams.toString()}`
   const darkkLogUrl = `https://darkk.net.ru/garbage/is-my-twitter-slow-or-what/log?${logParams.toString()}`
 
   return (
