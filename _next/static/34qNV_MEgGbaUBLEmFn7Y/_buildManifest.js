2f19d2ccbce69624f31bdf0f661aafff9b0f79e0
Andrey Viktorov <andreyviktorov@outlook.com>
2021-03-12 00:55:07 +0700

    Updates

--- _next/static/34qNV_MEgGbaUBLEmFn7Y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-458d88d28672e3364cbb.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-42c10ab6f9a8e262dd9a.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
