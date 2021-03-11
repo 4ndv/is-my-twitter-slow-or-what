ee80c36909474326e5048d164f7810328a6548f9
Andrey Viktorov <andreyviktorov@outlook.com>
2021-03-12 02:39:31 +0700

    Added height for image (hello Safari!)

--- components/TestImg.js
+++ components/TestImg.js
@@ -13,7 +13,7 @@ const TestImg = ({ type, src, onLoad }) => {
   const onLoadCallback = () => onLoad(type, +new Date() - startedAt)
 
   if (nonce) {
-    return <img className="inline-flex" src={`${src}?nocache=${nonce}`} onLoad={onLoadCallback} width={100} />
+    return <img className="inline-flex" src={`${src}?nocache=${nonce}`} onLoad={onLoadCallback} width={100} height={100} />
   }
 
   return null
