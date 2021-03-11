5d4d5e78222091b1683e735c24f19a4665d5ba6a
Andrey Viktorov <andreyviktorov@outlook.com>
2021-03-12 03:24:43 +0700

    sorry not sorry

--- components/Result.js
+++ components/Result.js
@@ -10,9 +10,21 @@ const Result = ({ test, control, controlTaco }) => {
   const controlTacoSpeed = SIZE / controlTaco * 1000
 
   let result = null
+  let ad = null
 
   if (testSpeed < 600 && controlSpeed > 600) {
     result = <b>замедлен</b>
+
+    // Один раз живём
+    ad = (
+      <blockquote>
+        <p>
+          Если хотите вернуть как раньше — установите <a target="_blank" href="https://my.redshieldvpn.com/?r=TWITTER">Red Shield VPN</a> всего за $39 на год с промокодом TWITTER.
+          <br />
+          <i>- на правах рекламы</i>
+        </p>
+      </blockquote>
+    )
   } else if (controlSpeed < 600) {
     result = <b>возможно замедлен (скорость контроля &lt; 600 kpbs)</b>
   } else {
@@ -28,6 +40,7 @@ const Result = ({ test, control, controlTaco }) => {
     <>
       <h3>&#8776; {testSpeed.toFixed(2)} kbps, {Math.round(testTime)} секунд(-а)</h3>
       <p>Скорее всего, твиттер у вас: <b>{result}</b></p>
+      {ad}
       <p>Контроль 1 (эта же картинка с другого сервера): {controlSpeed.toFixed(2)} kbps, {Math.round(controlTime)} секунд(-а)</p>
       <p>Контроль 2 (эта же картинка с другого сервера, в адресе которого есть <code>t.co</code>): {controlTacoSpeed.toFixed(2)} kbps, {Math.round(controlTacoTime)} секунд(-а)</p>
       <img src={logUrl} width={1} height={1} />
