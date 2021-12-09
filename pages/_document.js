3dd444fd9f84f796bc95c28d2251ea0f02d555d9
Andrey Viktorov <andreyviktorov@outlook.com>
2021-12-10 04:19:18 +0700

    added: analytics

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+
+          <script async src="https://a.shirou.lynx.pink/tracker.js" data-ackee-server="https://a.shirou.lynx.pink" data-ackee-domain-id="93e65cd3-a55b-41e7-957e-d6f51b25b43b" />
+        </body>
+      </Html>
+    )
+  }
+}
+
+export default MyDocument
