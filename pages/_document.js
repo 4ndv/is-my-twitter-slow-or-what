85fb8b28bd7b4f4108bb87fe4ad2c6dfae6b06ca
Andrey Viktorov <andreyviktorov@outlook.com>
2021-12-10 05:02:14 +0700

    changed: detailed analytics

--- pages/_document.js
+++ pages/_document.js
@@ -14,7 +14,13 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
 
-          <script async src="https://a.shirou.lynx.pink/tracker.js" data-ackee-server="https://a.shirou.lynx.pink" data-ackee-domain-id="93e65cd3-a55b-41e7-957e-d6f51b25b43b" />
+          <script
+            async
+            src="https://a.shirou.lynx.pink/tracker.js"
+            data-ackee-server="https://a.shirou.lynx.pink"
+            data-ackee-domain-id="93e65cd3-a55b-41e7-957e-d6f51b25b43b"
+            data-ackee-opts='{ "detailed": true }'
+          />
         </body>
       </Html>
     )
